7c1e07552ae0c63670fe7774c7297fe194e25488
NgVBinh <nguyenvanbinh06102002@gmail.com>
2023-07-01 20:18:00 +0700

    update

---
+++ CGO_Buoi01/CGO_Buoi01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ namespace : dự án lớn liên kết qua namespace
+references : chỗ gọi lệnh này
+ */
+
+namespace CGO_Buoi01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
