fedc06d576e7e982ee95cd582ff4ea52e7bef883
NgVBinh <nguyenvanbinh06102002@gmail.com>
2023-07-01 21:37:22 +0700

    update bt sử dụng toán tử

--- CGO_Buoi01/CGO_Buoi1_DoiTienTe/Program.cs
+++ CGO_Buoi01/CGO_Buoi1_DoiTienTe/Program.cs
@@ -15,10 +15,10 @@ namespace CGO_Buoi1_DoiTienTe
             float usd;
             double vnd;
             const float tigia = 23000; 
-            Console.Write("Nhập số USD cần đổi");
+            Console.Write("Nhập số USD cần đổi: ");
             usd=Convert.ToSingle(Console.ReadLine());
             vnd = (double)usd * tigia;
-            Console.WriteLine("Gía trị sau khi chuyển đổi:"+vnd);
+            Console.WriteLine("Số tiền VND bạn có là: "+vnd);
             Console.ReadKey();
         }
     }
