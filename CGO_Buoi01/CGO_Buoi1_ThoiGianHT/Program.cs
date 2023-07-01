6aa0f7693a49e2d433dc46910c0643f0059f5fe7
NgVBinh <nguyenvanbinh06102002@gmail.com>
2023-07-01 21:40:34 +0700

    update bt Hiển thị giờ hệ thống

---
+++ CGO_Buoi01/CGO_Buoi1_ThoiGianHT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi1_ThoiGianHT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("Date Time now is: "+dateTime);
+            Console.ReadKey();
+        }
+    }
+}
