d7be591ef0452ad32858b5fd416567090096d832
NgVBinh <nguyenvanbinh06102002@gmail.com>
2023-07-07 16:37:44 +0700

    update buoi3 So Nguyen To

---
+++ CGO_Buoi03/CGO_Buoi3_SoNguyenTo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi3_SoNguyenTo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Nhap so can kiem tra: ");
+            number=Convert.ToInt32(Console.ReadLine());
+
+            if (number < 2)
+            {
+                Console.WriteLine("{0} khong phai la so nguyen to",number);
+            }else if(number <= 3) {
+                Console.WriteLine("{0} la so nguyen to", number);
+            }
+            else
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("{0} khong phai la so nguyen to", number);
+                }
+                else
+                {
+                    bool flag = true;
+                    for(int i=3;i<Math.Sqrt(number);i+=2) {
+                        if (number % i == 0)
+                        {
+                            flag = false; break;
+                        }
+                    }
+                    if(flag) {
+                        Console.WriteLine("{0} la so nguyen to", number);
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} khong phai la so nguyen to", number);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
