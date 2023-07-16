fe40f2f72b19f28c61532a2bdc5379438475639d
NgVBinh <nguyenvanbinh06102002@gmail.com>
2023-07-16 22:05:08 +0700

    update bt buoi 07

---
+++ CGO_Buoi07/HinhChuNhat/HinhChuNhat/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HinhChuNhat
+{
+    internal class Program
+    {
+        public class Rectangle
+        {
+            private double width;
+            private double height;
+
+            public Rectangle() { }
+            public Rectangle(double width, double height)
+            {
+                this.width = width;
+                this.height = height;
+            }
+
+            public double GetArea()
+            {
+                return this.width * this.height;
+            }
+
+            public double GetPerimeter()
+            {
+                return (this.width+this.height)*2;
+            }
+            public string Display()
+            {
+                return "Rectangle: width "+this.width+" heigh "+this.height;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter the width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.Write("Enter the height:");
+            double height = Double.Parse(Console.ReadLine());
+
+
+
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Your Rectangle \n" + rectangle.Display());
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+
+            Console.ReadKey();
+        }
+    }
+}
